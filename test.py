6e1dd87ee7dbc94874de1096114466e5191ee893
Sergey KM <Sergey.Broks@yandex.ru>
2021-08-01 16:20:26 +0300

    test commit 2021-08-01

---
+++ test.py
@@ -0,0 +1,10 @@
+print('sdf')
+
+import math
+
+# Спросим, что хорошего в этой библиотеке
+print(math.__doc__)
+
+# Будет напечатано:
+# This module provides access to the mathematical functions
+# defined by the C standard. 
